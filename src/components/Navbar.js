60b57be481bd539b1cd271c44a74c7f94983e901
Jschles1 <jschles1@ramapo.edu>
2021-10-21 20:30:20 -0400

    Fix build error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as ReactRouterLink, useLocation } from 'react-router-dom';
@@ -125,7 +126,7 @@ const Navbar = () => {
 
     useEffect(() => {
         checkIfWalletIsConnected();
-    }, [checkIfWalletIsConnected]);
+    }, []);
 
     const accountAddress = truncateEthAddress(currentAccount);
 
