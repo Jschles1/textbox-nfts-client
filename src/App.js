cabfba6cba806121fe0beba37c6a874b5b01d528
Jschles1 <johnschlesinger5@gmail.com>
2021-11-15 20:22:05 -0500

    Lazy load pages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Layout from './components/Layout';
 
-import Home from './pages/Home';
-import Collection from './pages/Collection';
+const Home = React.lazy(() => import('./pages/Home'));
+const Collection = React.lazy(() => import('./pages/Collection'));
 
 /*
     TODO:
     - Add placeholder image for optimistic UI
-    - Add page for showing all mints
     - Add FAQ section
 */
 
 const App = () => {
     return (
-        <Router>
-            <Navbar />
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/collection" component={Collection} />
-                </Switch>
-            </Layout>
-        </Router>
+        <React.Suspense fallback={<>...</>}>
+            <Router>
+                <Navbar />
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/collection" component={Collection} />
+                    </Switch>
+                </Layout>
+            </Router>
+        </React.Suspense>
     );
 };
 
